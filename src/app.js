8a997eaca8c091416f52b5678c574b9d066cfb54
CserhatiDavid <CserhatiDavid4444@gmail.com>
2023-03-10 10:45:39 +0100

    tomb megirva

--- src/app.js
+++ src/app.js
@@ -8,3 +8,13 @@
 * Licenc: GNU GPL
 * Nem vállalunk felelősséget a rossz jegyekért
 */
+const tbody=document.querySelector("#tbody");
+
+var dolgozoLista=[
+    {name:"Pista",city:"Szolnok",salary:385},
+    {name:"Mari",city:"Szeged",salary:382},
+    {name:"Kati",city:"Szolnok",salary:355},
+    {name:"Dani",city:"Szeged",salary:384},
+    {name:"Lili",city:"Szolnok",salary:382}
+];
+console.log(tbody)
