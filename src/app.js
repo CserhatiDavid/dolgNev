5b69bca39f9923f156e9343e35a3949d47187f51
CserhatiDavid <CserhatiDavid4444@gmail.com>
2023-03-16 10:47:41 +0100

    database.json created

--- src/app.js
+++ src/app.js
@@ -8,13 +8,40 @@
 * Licenc: GNU GPL
 * Nem vállalunk felelősséget a rossz jegyekért
 */
-const tbody=document.querySelector("#tbody");
+const doc={ 
+    tbody:null
+}
+const state={
+    dolgozoLista:[]
+}
 
-var dolgozoLista=[
-    {name:"Pista",city:"Szolnok",salary:385},
-    {name:"Mari",city:"Szeged",salary:382},
-    {name:"Kati",city:"Szolnok",salary:355},
-    {name:"Dani",city:"Szeged",salary:384},
-    {name:"Lili",city:"Szolnok",salary:382}
-];
-console.log(tbody)
+
+window.addEventListener("load",()=>{
+    intit();
+    render();
+})
+function intit() {
+    doc.tbody=document.querySelector("#tbody");
+    state.dolgozoLista=[
+        {id:1, name:"Pista",city:"Szolnok",salary:385},
+        {id:2, name:"Mari",city:"Szeged",salary:382},
+        {id:3, name:"Kati",city:"Szolnok",salary:355},
+        {id:4, name:"Dani",city:"Szeged",salary:384},
+        {id:5, name:"Lili",city:"Szolnok",salary:382}
+    ];
+}
+function render() {
+    let rows="";
+    state.dolgozoLista.forEach((dolgozo)=>{
+        console.log(dolgozo.name)
+        rows+=`
+            <tr>
+                <td>${dolgozo.id}</td>
+                <td>${dolgozo.name}</td>
+                <td>${dolgozo.city}</td>
+                <td>${dolgozo.salary}</td>
+            </tr>
+        `;
+        doc.tbody.innerHTML=rows;
+    })
+}
